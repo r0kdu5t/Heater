98ab778100d55e5b0081cf1edddc4eb0676cf68f
Andrew Sands <andrew@theatrix.org.nz>
2017-06-28 09:58:59 +1200

    Version reporting and serial printing values from sensor.

--- Heater.ino
+++ Heater.ino
@@ -9,7 +9,6 @@
 #define ONE_WIRE_BUS 2  // Data wire is plugged into port 2 on the Arduino
 
 OneWire oneWire(ONE_WIRE_BUS);  // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
-
 DallasTemperature sensors(&oneWire);  // Pass our oneWire reference to Dallas Temperature.
 
 /*
@@ -19,23 +18,25 @@ void setup(void)
 {
   // start serial port
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
+  //Serial.println("Dallas Temperature IC Control Library Demo");
+  Serial.println( F("<r0kdu5t@theatrix.org.nz"));
 
-  // Example, not tested
+  // Example, not tested. See: http://forum.arduino.cc/index.php?topic=158014.0
 
   // Combined string in RAM
-  Serial.println( "Compiled: " __DATE__ ", " __TIME__ ", " __VERSION__);
+  //Serial.println( "Compiled: " __DATE__ ", " __TIME__ ", " __VERSION__);
 
   // The string in Flash
-  Serial.print( F("Compiled: ");
-                Serial.print( F(__DATE__));
-                Serial.print( F(", "));
-                Serial.print( F(__TIME__));
-                Serial.print( F(", "));
-                Serial.println( F(__VERSION__));
+  Serial.print( F("Compiled: "));
+  Serial.print( F(__DATE__));
+  Serial.print( F(", "));
+  Serial.print( F(__TIME__));
+  Serial.print( F(", "));
+  Serial.println( F(__FILE__));
+  delay(2000);
 
-                // Start up the library
-                sensors.begin();
+  // Start up the library
+  sensors.begin();
 }
 
 /*
@@ -52,4 +53,25 @@ void loop(void)
   // We use the function ByIndex, and as an example get the temperature from the first sensor only.
   Serial.print("Temperature for the device 1 (index 0) is: ");
   Serial.println(sensors.getTempCByIndex(0));
+  delay(1000);
+/*
+// Variables
+// =========
+// temperature
+// setTemp
+// HYSTERESIS
+// TRIAC_PIN
+
+// Code
+// Check if sensed value is less than set value minus HYSTERESIS 
+if (temperature < (setTemp - HYSTERESIS)) {
+  // Do what? Turn On Output
+  digitalWrite(TRIAC_PIN, HIGH);
+}
+// Check if sensed value is more than set value plus HYSTERESIS 
+if (temperature > (setTemp + HYSTERESIS)) {
+  // Do what? Turn Off Output
+  digitalWrite(TRIAC_PIN, LOW);
+}
+ */
 }
